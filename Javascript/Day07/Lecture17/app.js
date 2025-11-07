1ee6ffb11fbff4d5c2e2cfa2a055a1954e1496bf
Priyanshu <Preyanshukandari@gmail.com>
2025-11-07 20:20:02 +0530

    Lecture17 completed

---
+++ Javascript/Day07/Lecture17/app.js
@@ -0,0 +1,12 @@
+console.log(x); //undefined
+var x = 10;
+
+console.log(z); //error
+let z = 10
+
+
+//hoisting
+// Hoisting means JavaScript moves declarations (not assignments) to the top of their scope before code execution.
+
+// Think of it like this:
+// JS reads your code first, remembers all the declared variables and functions, and then runs the code line by line.
---
+++ Javascript/Day07/Lecture17/first.js
@@ -0,0 +1,12 @@
+greet()  //hello kandari
+
+function greet(){
+    console.log("hello kandari");
+}
+
+meet()  //error
+
+let meet = function(){
+    console.log("hello again");
+}
+
