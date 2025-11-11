7661f60f7944183d2c83b077cea2e1a4f2dea915
Priyanshu <Preyanshukandari@gmail.com>
2025-11-11 11:30:40 +0530

    lecture 19 completed

---
+++ Javascript/Day11/Lecture19/script.js
@@ -0,0 +1,7 @@
+const obj = document.getElementById("first");
+obj.style.fontFamily = "sans serif"
+obj.style.color = "blue"
+
+const obj1 = document.getElementsByClassName("header1");
+obj1[1].style.color = "green"
+obj1[0].style.backgroundColor = "yellow";
